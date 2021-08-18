3524e2a5a2f23ac17c1c69581240ac0b2696520b
maiada-ibrahim <maiada.ibrahim.27@icloud.com>
2021-08-19 00:47:08 +0300

    finishlab-09

--- src/App.js
+++ src/App.js
@@ -12,108 +12,103 @@ class App extends React.Component {
     this.state = {
       cityData: {},
       searchCity: '',
-      whether:[],
+      whether: [],
       showData: false,
-      showmap :false ,
-      errorshow:false,
-      citymoviesdata:[]
-      
+      showmap: false,
+      errorshow: false,
+      citymoviesdata: []
+
     }
 
   }
-  getlocation = async (event) => {
+  renderfun =  (event) => {
     event.preventDefault();
-    //--------------------------------------------------location
+    this.locationfun(event)
+    this.weatherfun()
+    this.moviesfun()
+  }
+
+  //-----------------------------------------------------------------location
+  locationfun=async(event)=>{
     await this.setState({
-       searchCity:this.state.searchCity= event.target.city.value
+      searchCity: this.state.searchCity = event.target.city.value
     })
- 
-    // console.log('llllll',event.target.city.value)
-   let locurl = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchCity}&format=json`
 
-   
-    
-   try{let resultData =  await axios.get(locurl)
-    console.log('llllll', resultData)
-
-    this.setState({
-      cityData:resultData.data[0],
-      showData: true,
-      showmap :true,
-      errorshow:false,
-    })
-    // console.log('llllll', this.state.cityData)
-   }
-   catch{
-    this.setState({
-      errorshow:true,
-      showData: false,
-      showmap :false,
-    })
-   }
+    let locurl = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchCity}&format=json`
 
 
-  // --------------------------------------------------------------------------weather
+
+    try {
+      let resultData = await axios.get(locurl)
+      console.log('llllll', resultData)
+
+      this.setState({
+        cityData: resultData.data[0],
+        showData: true,
+        showmap: true,
+        errorshow: false,
+      })
+    }
+    catch {
+      this.setState({
+        errorshow: true,
+        showData: false,
+        showmap: false,
+      })
+    }
+
+  }
+  //---------------------------------------------------------------weather
+  weatherfun = async () => {
     let weatherurl = `${process.env.REACT_APP_SERVER_LINK}/weather?city=${this.state.searchCity}&format=json`;
-    let resultData =  await axios.get(weatherurl)
+    let resultData = await axios.get(weatherurl)
     // console.log('kkkkkkkkk',resultData)
     await this.setState({
-      whether:resultData.data,
-      
-    })
-    
-    // ---------------------------------------------------------movies
-    let moviesurl = `${process.env.REACT_APP_SERVER_LINK}/movies?cityname=${this.state.searchCity}&format=json`;
-    let moviesresultData =  await axios.get(moviesurl)
-    console.log('kkkkkkkkk',moviesresultData.data)
-    await this.setState({
-      citymoviesdata:moviesresultData.data,
-      
+      whether: resultData.data,
+
     })
   }
-  // moviesfun=()=>{
+  //-----------------------------------------------------movies
+  moviesfun = async () => {
+    let moviesurl = `${process.env.REACT_APP_SERVER_LINK}/movies?cityname=${this.state.searchCity}&format=json`;
+    let moviesresultData = await axios.get(moviesurl)
+    console.log('kkkkkkkkk', moviesresultData.data)
+    await this.setState({
+      citymoviesdata: moviesresultData.data,
 
+    })
 
-  // }
+  }
+  //------------------------------------------------------------------------------render
   render() {
     return (
       <div>
-       <h2>City Explorer</h2>
-       <form  onSubmit={this.getlocation}>
-         <label > enter city</label>
-         <input type="text"  placeholder='enter city' name='city' />
-         <button> submite </button>
-         {/* {this.state.showData &&
-            <p> {this.state.searchCity} Lat:{this.state.cityData.lat} /Lon:{this.state.cityData.lon} </p>
-            
-          } */}
-          {/* {this.state.showmap && 
-             <img alt='' src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=15`} />
-          } */}
-          
-       </form>
+        <h2>City Explorer</h2>
+        <form onSubmit={this.renderfun}>
+          <label > enter city</label>
+          <input type="text" placeholder='enter city' name='city' />
+          <button> submite </button>
+        </form>
 
         <Card style={{ width: '18rem' }}>
-          
+          <Card.Body  >
+            <Card.Title> {this.state.showData &&
+              <p> {this.state.searchCity} Lat:{this.state.cityData.lat} /Lon:{this.state.cityData.lon} </p>
 
-<Card.Body  >
-  <Card.Title> {this.state.showData &&
-            <p> {this.state.searchCity} Lat:{this.state.cityData.lat} /Lon:{this.state.cityData.lon} </p>
-            
-          }</Card.Title>
-  <Card.Title> {this.state.errorshow &&
-            <p> you have some error </p>
-            
-          }</Card.Title>
- 
-  {this.state.showmap &&
-    <Card.Img variant="top" src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=15`} alt='' />
-  }
+            }</Card.Title>
+            <Card.Title> {this.state.errorshow &&
+              <p> you have some error </p>
+
+            }</Card.Title>
+
+            {this.state.showmap &&
+              <Card.Img variant="top" src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=15`} alt='' />
+            }
           </Card.Body>
 
         </Card>
-<Wether whether = {this.state.whether} />
-<Movies citymoviesdata = {this.state.citymoviesdata} />
+        <Wether whether={this.state.whether} />
+        <Movies citymoviesdata={this.state.citymoviesdata} />
 
       </div>
     )
